c2d1dfa0731ead733046a05c97c21025e49dc29d
Gino Wu <gino_wu@trendmicro.com>
2022-01-17 21:49:11 +0800

    fix: check victory

--- src/App.js
+++ src/App.js
@@ -5,26 +5,30 @@ import styles from './App.module.css';
 import O from './components/O';
 import X from './components/X';
 
-// { // Redux store
-// 	games: [gameId, gameId],
-// }
-
-
-// interface Game {
-// 	id: number;
-// 	players: Player[];
-// 	cells: Number[]
-// }
-
 const initialState = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+const LAST_STEP = 8;
+
 
 function App() {
   const [cells, setCells] = useState([...initialState]); // 0 = no, 1 == x, 2 == o
   const [step, setStep] = useState(0);
+  const player = step % 2 + 1;
+
+  const isVictory = (cells) => {
+    const positions = [
+      [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6],
+      [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]
+    ];
+
+    const isRowComplete = row => {
+      const symbols = row.map(i => cells[i]);
+      return symbols.every(i => i !== null && i === symbols[0] && i !== 0);
+    };
+
+    return positions.map(isRowComplete).some(i => i === true);
+  }
 
   const handleClickGrid = (cellIndex) => {
-    const player = step % 2 + 1;
-
     if (cells[cellIndex]) { return; }
 
     cells[cellIndex] = player;
@@ -50,6 +54,7 @@ function App() {
           </div>
         )}
       </div>
+      <div>{isVictory(cells) && `player#${player} win`}</div>
     </div>
   );
 }
