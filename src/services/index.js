085d4b6b15a54bb7f50f8784c80d6eded761b0b9
Winter Weng <yichinweng@gmail.com>
2022-04-18 22:25:10 +0800

    chore: fix indent

--- src/services/index.js
+++ src/services/index.js
@@ -1,19 +1,19 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const gameApis = createApi({
-    baseQuery: fetchBaseQuery({ baseUrl: '/api'}),
-    endpoints: (builder) => ({
-        createGame: builder.mutation({
-            query: () => ({
-                url: '/game',
-                method: 'POST',
-            })
-        }),
-        getGame: builder.query({
-            query: (id) => `/game/${id}`
-        })
-    })
-    // query: (id) => ({ url: `post/${id}` }),
+  baseQuery: fetchBaseQuery({ baseUrl: '/api' }),
+  endpoints: (builder) => ({
+    createGame: builder.mutation({
+      query: () => ({
+        url: '/game',
+        method: 'POST',
+      }),
+    }),
+    getGame: builder.query({
+      query: (id) => `/game/${id}`,
+    }),
+  }),
+  // query: (id) => ({ url: `post/${id}` }),
 });
 
 export const { useGetGameQuery, useCreateGameMutation } = gameApis;
